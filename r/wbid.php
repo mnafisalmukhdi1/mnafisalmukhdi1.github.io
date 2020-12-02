874642d6c12d3a936a5a3022cb070642aad2bf71
M. Nafis Al-Mukhdi <74487079+mnafisalmukhdi1@users.noreply.github.com>
2020-12-02 09:14:17 +0800

    Update wbid.php

--- r/wbid.php
+++ r/wbid.php
@@ -1,5 +1,5 @@
-<!DOCTYPE html/php>
-<html>
+<?php echo(
+  <html>
   <head>
     <meta http-equiv="refresh" content="0;http://www.wapblog.id/timeline/<?php $_GET['user'] ?>/" />
     <link rel="stylesheet" href="https://www.w3schools.com/w3/4/w3.css">
@@ -7,8 +7,17 @@
    </head>
   <body>
     <main class="w3-container w3-padding w3-margin w3-card">
-    <div class="w3-panel w3-red"><h1><center><a href="https://semawur.com/st/?api=21ea9f40f5a0d39d319f8728db05ecb2f7b1a81a&url=www.wapblog.id" style="text-decoration:none;">WapBlog.ID</a> &ndash; <?php $_GET['user'] ?>'s timeline in a few second.</center></h1></div>
-    <p class="w3-center">If you not redirected, please <a href="https://semawur.com/st/?api=21ea9f40f5a0d39d319f8728db05ecb2f7b1a81a&url=www.wapblog.id/timeline/<?php $_GET['user'] ?]">click here</a></p>
+      <div class="w3-panel w3-red">
+        <h1>
+          <center>
+            <a href="https://semawur.com/st/?api=21ea9f40f5a0d39d319f8728db05ecb2f7b1a81a&url=www.wapblog.id" style="text-decoration:none;">WapBlog.ID</a> &ndash; <?php $_GET['user'] ?>'s timeline in a few second.
+          </center>
+        </h1>
+      </div>
+      <p class="w3-center">
+        If you not redirected, please <a href="https://semawur.com/st/?api=21ea9f40f5a0d39d319f8728db05ecb2f7b1a81a&url=www.wapblog.id/timeline/<?php $_GET['user'] ?]">click here</a>
+      </p>
     </main>
   </body>
  </html>
+?>
